029d28e2e229f734b203f42ae6fe13d42e39d4bd
LohaneSthefany-2 <loohane456@outlook.com>
2026-02-18 20:25:42 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,29 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Questão 5</title>
+    <title>Questão 12</title>
 </head>
 <body>
     <form method="post" action="">
-        <label>nome do aluno</label>
-        <input type="text" name="nome" required><br><br>
-        <label>1 nota:</label>
-        <input type="number" name="num1" required><br><br>
-        <label>2 nota:</label>
-        <input type="number" name="num2" required><br><br>
-        <label>3 nota:</label>
-        <input type="number" name="num3" required><br><br>
-        <input type="submit" name="resultado" value="resultado">
+        <label>custo de fábrica:</label>
+        <input type="number" name="num" required><br><br>
     </form>
     <?php
-        $nome = $_POST['nome'];
-        $primeiranota = $_POST['num1'];
-        $segundanota = $_POST['num2'];
-        $terceiranota = $_POST['num3'];
-        $soma = $primeiranota + $segundanota + $terceiranota;
-        $media = $soma / 3;
-        echo "<h3>a média do aluno $nome é $media;<h3>";
-
+        $custo = $_POST['num'];
+        $imposto = $custo * (45/100);
+        $resultado = $custo + $imposto;
+        $distribuidor = $resultado * (28/100);
+        $preco = $resultado + $distribuidor;
+        echo "O custo de fábrica é: $custo <br>";
     ?>
     
 </body>
